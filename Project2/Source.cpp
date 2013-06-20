bf4334c3cbeca6c7693d19352fdfe75bbc604fef
nOOb3167 <nOOb3167@gmail.com>
2013-06-21 00:59:34 +0200

    Fix horrible bugs in Bitmap creation

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -14,66 +14,6 @@
 using namespace std;
 using namespace oglplus;
 
-void Demo() {
-	int r;
-	HDC hdc;
-	
-	HBITMAP hBmp = (HBITMAP)LoadImageA(NULL, "C:\\Users\\Andrej\\Documents\\BlendTmp\\bTest01.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-	assert(hBmp);
-
-	hdc = CreateCompatibleDC(NULL);
-	assert(hdc);
-
-	BITMAPINFO *bI2 = (BITMAPINFO *)calloc(1, sizeof *bI2);
-	bI2->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-
-	r = GetDIBits(hdc, hBmp, 0, 0, NULL, bI2, DIB_RGB_COLORS);
-	assert(r);
-}
-
-GLubyte * CreateImageBufRGB(const char *fname, int *w, int *h) {
-	int r;
-	HBITMAP hBmp;
-	HDC hdc;
-	DWORD dwBmpSize;
-	GLubyte *buf;
-
-	assert(w && h);
-
-	hBmp = (HBITMAP)LoadImageA(NULL, fname, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-	assert(hBmp);
-
-	hdc = CreateCompatibleDC(NULL);
-	assert(hdc);
-
-	BITMAPINFO bInfo = {0};
-	bInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-
-	r = GetDIBits(hdc, hBmp, 0, 0, NULL, &bInfo, DIB_RGB_COLORS);
-	assert(r);
-
-	/* Conservatively assume 4 components, 32 bpp, rows aligned to pixel boundary. */
-	assert(bInfo.bmiHeader.biBitCount == 32);
-	dwBmpSize = ((bInfo.bmiHeader.biWidth * bInfo.bmiHeader.biBitCount + 31) / 32) * 4 * bInfo.bmiHeader.biHeight;
-
-	buf = new GLubyte[dwBmpSize];
-	assert (buf);
-
-	r = GetDIBits(hdc, hBmp, 0, bInfo.bmiHeader.biHeight, buf, &bInfo, DIB_RGB_COLORS);
-	assert(r);
-
-	r = DeleteDC(hdc);
-	assert(r);
-
-	r = DeleteObject(hBmp);
-	assert(r);
-
-	*w = bInfo.bmiHeader.biWidth;
-	*h = bInfo.bmiHeader.biHeight;
-
-	return buf;
-};
-
 class SaneStoreState {
 public:
 	GLint swapbytes, lsbfirst, rowlength, skiprows, skippixels, alignment;
@@ -104,12 +44,64 @@ public:
 	}
 };
 
+GLubyte * CreateImageBufRGB(const char *fname, int *w, int *h) {
+	int r;
+	HBITMAP hBmp;
+	HDC hdc;
+	int tmpW, tmpH;
+	GLubyte *buf;
+
+	assert(w && h);
+
+	hBmp = (HBITMAP)LoadImageA(NULL, fname, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+	assert(hBmp);
+
+	hdc = CreateCompatibleDC(NULL);
+	assert(hdc);
+
+	BITMAPINFO bInfo = {0};
+	bInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+
+	r = GetDIBits(hdc, hBmp, 0, 0, NULL, &bInfo, DIB_RGB_COLORS);
+	assert(r);
+
+	/* Only interested in Width and Height, now prime the structure for the 2nd GetDIBits call.
+	   http://msdn.microsoft.com/en-us/library/windows/desktop/dd183376%28v=vs.85%29.aspx
+	       Says if biBitCount = 32 && biCompression = BI_RGB then bmiColors will be empty. */
+	tmpW = bInfo.bmiHeader.biWidth;
+	tmpH = bInfo.bmiHeader.biHeight;
+
+	assert(bInfo.bmiHeader.biPlanes == 1);
+	assert(bInfo.bmiHeader.biBitCount == 32);
+	bInfo.bmiHeader.biCompression = BI_RGB;
+	bInfo.bmiHeader.biSizeImage = 0;
+	bInfo.bmiHeader.biClrUsed = 0;
+	bInfo.bmiHeader.biClrImportant = 0;
+
+	buf = new GLubyte[bInfo.bmiHeader.biWidth * bInfo.bmiHeader.biHeight * 4];
+	assert (buf);
+
+	r = GetDIBits(hdc, hBmp, 0, bInfo.bmiHeader.biHeight, buf, &bInfo, DIB_RGB_COLORS);
+	assert(r);
+
+	r = DeleteDC(hdc);
+	assert(r);
+
+	r = DeleteObject(hBmp);
+	assert(r);
+
+	*w = tmpW;
+	*h = tmpH;
+
+	return buf;
+};
+
 oglplus::Texture CreateTexture(const char *fname) {
 	int w, h;
 	SaneStoreState sss;
 
 	unique_ptr<GLubyte[]> buf(CreateImageBufRGB(fname, &w, &h));
-	shared_ptr<oglplus::images::Image> im = make_shared<oglplus::images::Image>(w, h, 1, 3, buf.get());
+	shared_ptr<oglplus::images::Image> im = make_shared<oglplus::images::Image>(w, h, 1, 4, buf.get());
 
 	oglplus::Texture tex;
 	tex.Active(0);
@@ -117,18 +109,58 @@ oglplus::Texture CreateTexture(const char *fname) {
 	tex.MinFilter(oglplus::TextureTarget::_2D, oglplus::TextureMinFilter::Nearest);
 	tex.MagFilter(oglplus::TextureTarget::_2D, oglplus::TextureMagFilter::Nearest);
 	tex.Image2D(oglplus::TextureTarget::_2D, *im);
+	tex.Unbind(oglplus::TextureTarget::_2D);
 
 	return tex;
 }
 
-int main() {
-	aiLogStream stream = aiGetPredefinedLogStream(aiDefaultLogStream_STDOUT,NULL);
-	aiScene *scene = const_cast<aiScene *>(aiImportFile("C:\\Users\\Andrej\\Documents\\BlendTmp\\mCube01.dae", aiProcessPreset_TargetRealtime_MaxQuality));
-	assert(scene);
+class Ex1 {
+public:
+	aiScene *scene;
 
-	Demo();
+	oglplus::Texture tex;
 
-	oglplus::Texture tex = CreateTexture("C:\\Users\\Andrej\\Documents\\BlendTmp\\bTest01.bmp");
+	Ex1() {
+		scene = const_cast<aiScene *>(aiImportFile("C:\\Users\\Andrej\\Documents\\BlendTmp\\mCube01.dae", aiProcessPreset_TargetRealtime_MaxQuality));
+		assert(scene);
+
+		tex = CreateTexture("C:\\Users\\Andrej\\Documents\\BlendTmp\\bTest01.bmp");
+	}
+
+	void Display() {
+	}
+};
+
+static Ex1 *gEx = nullptr;
+
+void display(void) {
+	oglplus::Context gl;
+	gl.ClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+	gl.Clear().ColorBuffer().DepthBuffer();
+
+	assert(gEx);
+	gEx->Display();
+
+	glutSwapBuffers();
+}
+
+int main(int argc, char **argv) {
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(100, 100);
+	glutInitContextVersion (3,3);
+	glutInitContextProfile (GLUT_CORE_PROFILE);
+	glewExperimental=TRUE;
+	glutCreateWindow("OGLplus");
+	assert (glewInit() == GLEW_OK);
+	glGetError();
+
+	gEx = new Ex1();
+
+	glutDisplayFunc(display);
+	glutMainLoop();
 
 	return EXIT_SUCCESS;
 }
